ba45c723fde5f54e50c08d4db8a819ff6af1571e
André Lucas <78483723+Dev-Dreean@users.noreply.github.com>
2025-10-16 23:51:25 -0300

    Corrigir migration 2025_10_06_optimizar_indices_dropdowns.php para usar classe anônima e evitar erro de nome duplicado no Laravel.

--- database/migrations/2025_10_06_optimizar_indices_dropdowns.php
+++ database/migrations/2025_10_06_optimizar_indices_dropdowns.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class OptimizarIndicesDropdowns extends Migration
-{
+return new class extends Migration {
     public function up()
     {
         // Locais
@@ -117,4 +116,4 @@ class OptimizarIndicesDropdowns extends Migration
             }
         }
     }
-}
+};
