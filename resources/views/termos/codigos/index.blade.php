302a801dc25a49e83c0d060ebc26a380ad2135e9
André Lucas <78483723+Dev-Dreean@users.noreply.github.com>
2026-02-03 14:04:13 -0300

    fix: Corrigir atributo 'name' corrompido no formulário de códigos (CÃ³digo -> codigo)

--- resources/views/termos/codigos/index.blade.php
+++ resources/views/termos/codigos/index.blade.php
@@ -25,7 +25,7 @@
                 <div class="flex items-end gap-2 mb-4">
                     <div>
                         <label class="text-sm text-gray-700">Novo código</label>
-                        <input type="number" name="CÃ³digo" form="formNovoCodigo" value="{{ old('codigo', $sugestao) }}" class="h-10 px-3 border border-gray-300 rounded w-40" />
+                        <input type="number" name="codigo" form="formNovoCodigo" value="{{ old('codigo', $sugestao) }}" class="h-10 px-3 border border-gray-300 rounded w-40" />
                     </div>
                     <form method="POST" action="{{ route('termos.codigos.salvar') }}" id="formNovoCodigo">
                         @csrf
